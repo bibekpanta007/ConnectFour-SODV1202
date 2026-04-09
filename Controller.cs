aa22e5fdc15b0286ddd86d79ee3b3febf8f2aa22
bibekpanta007 <bibekpanta183@gmail.com>
2026-04-09 13:46:52 -0600

    Cleaned project and updated Controller logic

--- Controller.cs
+++ Controller.cs
@@ -6,18 +6,71 @@ namespace Connect_Four
     {
         private Model model;
         private View view;
+        private Player player1;
+        private Player player2;
 
-        // Constructor
         public Controller()
         {
             model = new Model();
             view = new View();
+
+            player1 = new HumanPlayer("Player 1", 'X');
+            player2 = new HumanPlayer("Player 2", 'O');
         }
 
-        // This is called by Program.cs
         public void StartGame()
         {
-            view.ShowMessage("Game is starting...");
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                model = new Model(); // reset board
+                Player currentPlayer = player1;
+                bool gameOver = false;
+
+                view.ShowMessage("Welcome to Connect Four!");
+
+                while (!gameOver)
+                {
+                    view.DisplayBoard(model.Board);
+
+                    int column = currentPlayer.MakeMove();
+
+                    if (model.MakeMove(currentPlayer.Symbol, column))
+                    {
+                        if (model.CheckWin(currentPlayer.Symbol))
+                        {
+                            view.DisplayBoard(model.Board);
+                            view.ShowMessage($"{currentPlayer.Name} wins!");
+                            gameOver = true;
+                        }
+                        else if (model.CheckDraw())
+                        {
+                            view.DisplayBoard(model.Board);
+                            view.ShowMessage("It's a draw!");
+                            gameOver = true;
+                        }
+                        else
+                        {
+                            currentPlayer = (currentPlayer == player1) ? player2 : player1;
+                        }
+                    }
+                    else
+                    {
+                        view.ShowMessage("Column full or invalid move. Try again.");
+                    }
+                }
+
+                Console.WriteLine("Play again? (y/n): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input != "y")
+                {
+                    playAgain = false;
+                }
+            }
+
+            view.ShowMessage("Thanks for playing!");
         }
     }
 }
--- obj/Debug/net8.0/Connect_Four.AssemblyInfo.cs
+++ obj/Debug/net8.0/Connect_Four.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connect_Four")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137d6cd925a2d155ffba23ddd0194fd46f90ff3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connect_Four")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connect_Four")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
